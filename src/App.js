ef64fdf4015880fcc5c2efa478af4b807914cbd2
bundance <mpevans@gmail.com>
2018-10-01 15:50:53 +0100

    render email when user presses on message

--- src/App.js
+++ src/App.js
@@ -3,32 +3,68 @@ import {
   FlatList,
   StyleSheet,
   Text,
+  TouchableWithoutFeedback,
   View
 } from 'react-native';
 import { connect } from 'react-redux';
 import { getChatLog } from './state/messages/messages.actions';
 import { getMembers } from './state/members/members.actions';
-import { selectMessages } from './state/messages/messages.selectors';
+import { getRichMessages } from './state/messages/messages.selectors';
 import { selectMembers } from './state/members/members.selectors';
 
+
+class MyListItem extends React.PureComponent {
+  
+  state = {
+    showEmail: false
+  }
+  
+  changeShowEmailState = showEmail => this.setState({ showEmail });
+
+  render() {
+    const { item } = this.props;
+    const { showEmail } = this.state;
+    
+    return (
+      <TouchableWithoutFeedback 
+        onPressIn={() => this.changeShowEmailState(true)}
+        onPressOut={() => this.changeShowEmailState(false)}
+      >
+        <View>
+          <Text style={styles.welcome}>
+            { item.messageBody.message }
+          </Text>
+          { showEmail && <Text style={styles.email}>{item && item.member && item.member.email}</Text> }
+        </View>
+      </TouchableWithoutFeedback>
+    );
+  }
+}
+
+
 class App extends Component {
+  
   componentDidMount(){
     this.props.getChatLog();
     this.props.getMembers();
   }
 
-  keyExtractor = item => (item && item.id)
+  keyExtractor = item => (item && item.messageBody.id)
+
+  renderItem = ({item}) => (
+    <MyListItem item={item}/>
+  );
 
   render() {
     const { members, messages } = this.props;
-    console.log({ members });
     
     return (
       <View style={styles.container}>
         <FlatList
           data={messages}
+          extraData={this.state}
           keyExtractor={this.keyExtractor}
-          renderItem={({ item }) => (<Text style={styles.welcome}>{item.message}</Text>) }
+          renderItem={this.renderItem} 
         />
       </View>
     );
@@ -37,7 +73,7 @@ class App extends Component {
 
 const mapStateToProps = state => ({
   members: selectMembers(state),
-  messages: selectMessages(state)
+  messages: getRichMessages(state)
 });
 
 const mapDispatchToProps = ({
@@ -54,13 +90,16 @@ export default connect(
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
     backgroundColor: '#F5FCFF',
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
+    textAlign: 'left',
     margin: 10,
+  },
+  email: {
+    fontSize: 12
   }
 });
--- src/state/members/members.selectors.js
+++ src/state/members/members.selectors.js
@@ -1,1 +1,6 @@
 export const selectMembers = state => state.members;
+
+export const getMemberById = id => createSelector(
+    [selectMembers],
+    members => members.find(member => member.id === id)
+);
